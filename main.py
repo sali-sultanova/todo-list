aee2eb3c37f5717675234dc69f28ebb4da5abedf
sali-sultanova1 <salli.sultanova@gmail.com>
2026-05-10 19:04:35 +0300

    add choice 4

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 tasks = []
 while True:
-    choice = int(input("1- Добавить, 2-Просмотреть, 3-Удалить"))
+    choice = int(input("1- Добавить, 2-Просмотреть, 3-Удалить, 4-Изменить"))
     if choice == 1:
         pass
     elif choice == 2:
-
+        print(tasks)
     elif choice == 3:
+        pass
+    elif choice == 4:
         pass
     else:
         print("error")
