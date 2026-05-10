a465706da5cfc99d85527e6ddc141fed7ca86d27
yap1ng <mooncakeks@gmail.com>
2026-05-10 19:38:18 +0300

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 tasks = []
+
+def delete_task(tasks):
+    print(tasks)
+    tasks.pop(int(input("Номер задачи: ")) - 1)   
+
 while True:
     choice = int(input("1- Добавить, 2-Просмотреть, 3-Удалить, 4-Изменить"))
     if choice == 1:
@@ -6,8 +11,11 @@ while True:
     elif choice == 2:
         print(tasks)
     elif choice == 3:
-        pass
+        delete_task(tasks)
     elif choice == 4:
         pass
     else:
         print("error")
+
+
+
