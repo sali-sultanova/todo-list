e8df1a42350b5c1dd25c58476c6468a8429ec770
sali-sultanova1 <salli.sultanova@gmail.com>
2026-05-10 18:48:30 +0300

    first

---
+++ main.py
@@ -0,0 +1,10 @@
+while True:
+    choice = int(input("1- Добавить, 2-Просмотреть, 3-Удалить"))
+    if choice == 1:
+        pass
+    elif choice == 2:
+        pass
+    elif choice == 3:
+        pass
+    else:
+        print("error")
