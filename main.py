1311a960952112dc5422f6d76e156e1d0dc1c16f
Magomedova799 <azhai054@mail.ru>
2026-05-10 19:53:45 +0300

    сохраняю внутри папки

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 tasks = []
+
+
+def add_task():
+    task = input("Введите задачу: ")
+    tasks.append(task)
+    print("Задача добавлена!")
+
+
 while True:
     choice = int(input("1- Добавить, 2-Просмотреть, 3-Удалить, 4-Изменить"))
     if choice == 1:
-        pass
+        add_task()
     elif choice == 2:
         print(tasks)
     elif choice == 3:
