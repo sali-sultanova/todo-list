931451b3b5c717bda0d5727bc462aad47f448848
sali-sultanova1 <salli.sultanova@gmail.com>
2026-05-10 19:37:58 +0300

    choice 2 and 4

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
+def show_tasks(tasks):
+    if not tasks:
+        print("Список задач пуст")
+    else:
+        for i, task in enumerate(tasks, 1):
+            print(f"{i}. {task}")
+
+def edit_task(tasks):
+    print(f"Ваши задачи: ")
+    show_tasks(tasks)
+    num = int(input("Введите номер задачи для изменения: "))
+    if 1 <= num <= len(tasks):
+        new_task = input("Введите измененный текст задачи: ")
+        tasks[num-1] = new_task
+        print("Задача обновлена")
+    else:
+        print("Неправильный номер задачи")
+
+
 tasks = []
 while True:
     choice = int(input("1- Добавить, 2-Просмотреть, 3-Удалить, 4-Изменить"))
     if choice == 1:
         pass
     elif choice == 2:
-        print(tasks)
+        show_tasks(tasks)
     elif choice == 3:
         pass
     elif choice == 4:
-        pass
+        edit_task(tasks)
     else:
         print("error")
+
