d678360e4a72bd7480648eda6547c7d90b9a0b5a
sali-sultanova1 <salli.sultanova@gmail.com>
2026-05-10 18:57:20 +0300

    add tasks

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+tasks = []
 while True:
     choice = int(input("1- Добавить, 2-Просмотреть, 3-Удалить"))
     if choice == 1:
         pass
     elif choice == 2:
-        pass
+
     elif choice == 3:
         pass
     else:
